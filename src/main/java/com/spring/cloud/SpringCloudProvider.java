3422f0fad9c5cf260ae69bfbd772a0fea1baeb18
x00385752 <xianshun@huawei.com>
2019-03-11 20:49:27 +0800

    add

---
+++ src/main/java/com/spring/cloud/SpringCloudProvider.java
@@ -0,0 +1,27 @@
+package com.spring.cloud;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class SpringCloudProvider {
+    
+    @Value("${foo.db}")
+    private String foodb;
+    
+	@GetMapping("/hello")
+	public String hello(){
+		System.out.println("hello ");
+		return "hello,I am springcloud provider.";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudProvider.class, args);
+	}
+}
